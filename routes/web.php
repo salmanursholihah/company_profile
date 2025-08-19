51ecf89c5400e2e93c2d311cb62747accc702d8b
salmanursholihah <salmanursholihah@gmail.com>
2025-08-19 16:00:37 +0700

    project awal

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
+Route::get('/team', function () {
+    return view('team');
+})->name('team');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
